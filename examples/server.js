700d0b6475dbd4d08839f901a585ff0e20db8171
wenruo0522 <zjm023@mail.ustc.edu.cn>
2022-02-18 19:54:33 +0800

    feat: 创建 demo 文件

---
+++ examples/server.js
@@ -0,0 +1,42 @@
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const webpackConfig = require('./webpack.config')
+
+const app = express()
+const compile = webpack(webpackConfig)
+
+const router = express.Router()
+
+router.get('/simple/get', (req, res) => {
+    res.json({
+      msg: `hello world`
+    })
+})
+
+app.use(router)
+
+app.use(webpackDevMiddleware(compile, {
+  publicPath: '/__build__/',
+  stats: {
+    colors: true,
+    chunk: false
+  }
+}))
+
+app.use(webpackHotMiddleware(compile))
+
+app.use(express.static(__dirname))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const port = process.env.PORT || 8080
+
+module.exports = app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}, Ctrl + C to stop`)
+})
+
