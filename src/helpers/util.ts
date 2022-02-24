30bd3a3ff19293121d754a52f5d0aafe68ecd4da
wenruo0522 <zjm023@mail.ustc.edu.cn>
2022-02-24 14:22:56 +0800

    feat: isPlainObject && transformRequestData

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -4,6 +4,10 @@ export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
-export function isObject(val: any): val is object {
-  return val !== null && typeof val === 'object'
+// export function isObject(val: any): val is object {
+//   return val !== null && typeof val === 'object'
+// }
+
+export function isPlainObject(val: any): val is Object {
+  return toString.call(val) === '[object Object]'
 }
