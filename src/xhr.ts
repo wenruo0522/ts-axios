778a5dd111b63bc975e1314e1e34bc2d3ff92422
wenruo0522 <376567610@qq.com>
2022-02-25 00:54:29 +0800

    feat: processHeaders && setRequestHeaders

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,8 +1,16 @@
 import { AxiosRequestConfig } from './types'
 
 export default function xhr(config: AxiosRequestConfig): void {
-  const { data = null, url, method = 'get' } = config
+  const { data = null, url, method = 'get', headers } = config
   const request = new XMLHttpRequest()
+
+  Object.keys(headers).forEach(name => {
+    if (data === null && name.toLowerCase() === 'content-type') {
+      delete headers[name]
+    } else {
+      request.setRequestHeader(name, headers[name])
+    }
+  })
 
   request.open(method.toUpperCase(), url, true)
 
