0524369c2dbec8b4d6c7ea7c3b03acdd7a512587
wenruo0522 <zjm023@mail.ustc.edu.cn>
2022-02-24 19:44:17 +0800

    feat: transformRequestData demo

--- examples/server.js
+++ examples/server.js
@@ -21,6 +21,23 @@ router.get('/base/get', (req, res) => {
   res.json(req.query)
 })
 
+router.post('/base/post', (req, res) => {
+  res.json(req.body)
+})
+
+router.post('/base/buffer', (req, res) => {
+  let msg = []
+  req.on('data', chunk => {
+    if (chunk) {
+      msg.push(chunk)
+    }
+  })
+  req.on('end', () => {
+    let buf = Buffer.concat(msg)
+    res.json(buf.toJSON())
+  })
+})
+
 app.use(router)
 
 app.use(webpackDevMiddleware(compile, {
