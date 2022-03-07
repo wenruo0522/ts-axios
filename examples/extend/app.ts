12cdc1a4310988916dc06bd72179304bd253edc1
wenruo0522 <zjm023@mail.ustc.edu.cn>
2022-03-07 13:14:30 +0800

    feat: extend demo

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -8,3 +8,24 @@ axios({
   }
 })
 
+axios.request({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'hello'
+  }
+})
+
+axios.get('/extend/get')
+
+axios.options('/extend/options')
+
+axios.delete('/extend/delete')
+
+axios.head('/extend/head')
+
+axios.post('/extend/post', { msg: 'post' })
+
+axios.put('/extend/put', { msg: 'put' })
+
+axios.patch('/extend/patch', { msg: 'patch' })
--- examples/server.js
+++ examples/server.js
@@ -11,51 +11,13 @@ const compile = webpack(webpackConfig)
 
 const router = express.Router()
 
-router.get('/simple/get', (req, res) => {
-  res.json({
-    msg: `hello world`
-  })
-})
+registerSimpleRouter()
 
-router.get('/base/get', (req, res) => {
-  res.json(req.query)
-})
+registerBaseRouter()
 
-router.post('/base/post', (req, res) => {
-  res.json(req.body)
-})
+registerErrorRouter()
 
-router.post('/base/buffer', (req, res) => {
-  let msg = []
-  req.on('data', chunk => {
-    if (chunk) {
-      msg.push(chunk)
-    }
-  })
-  req.on('end', () => {
-    let buf = Buffer.concat(msg)
-    res.json(buf.toJSON())
-  })
-})
-
-router.get('/error/get', (req, res) => {
-  if (Math.random() > 0.5) {
-    res.json({
-      msg: `hello world`
-    })
-  } else {
-    res.status(500)
-    res.end()
-  }
-})
-
-router.get('/error/timeout', (req, res) => {
-  setTimeout(() => {
-    res.json({
-      msg: `hello world`
-    })
-  }, 3000)
-})
+registerExtendRouter()
 
 
 app.use(webpackDevMiddleware(compile, {
@@ -79,4 +41,91 @@ const port = process.env.PORT || 8080
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl + C to stop`)
 })
+
+function registerSimpleRouter() {
+  router.get('/simple/get', (req, res) => {
+    res.json({
+      msg: `hello world`
+    })
+  })
+}
+
+function registerBaseRouter() {
+  router.get('/base/get', (req, res) => {
+    res.json(req.query)
+  })
+
+  router.post('/base/post', (req, res) => {
+    res.json(req.body)
+  })
+
+  router.post('/base/buffer', (req, res) => {
+    let msg = []
+    req.on('data', chunk => {
+      if (chunk) {
+        msg.push(chunk)
+      }
+    })
+    req.on('end', () => {
+      let buf = Buffer.concat(msg)
+      res.json(buf.toJSON())
+    })
+  })
+}
+
+function registerErrorRouter() {
+  router.get('/error/get', (req, res) => {
+    if (Math.random() > 0.5) {
+      res.json({
+        msg: `hello world`
+      })
+    } else {
+      res.status(500)
+      res.end()
+    }
+  })
+
+  router.get('/error/timeout', (req, res) => {
+    setTimeout(() => {
+      res.json({
+        msg: `hello world`
+      })
+    }, 3000)
+  })
+}
+
+function registerExtendRouter() {
+  router.get('/extend/get', (req, res) => {
+    res.json({
+      msg: 'hello world'
+    })
+  })
+
+  router.options('/extend/options', (req, res) => {
+    res.end()
+  })
+
+  router.delete('/extend/delete', (req, res) => {
+    res.end()
+  })
+
+  router.head('/extend/head', (req, res) => {
+    res.end()
+  })
+
+  router.post('/extend/post', (req, res) => {
+    res.json(req.body)
+  })
+
+  router.put('/extend/put', (req, res) => {
+    res.json(req.body)
+  })
+
+  router.patch('/extend/patch', (req, res) => {
+    res.json(req.body)
+  })
+
+
+
+}
 
