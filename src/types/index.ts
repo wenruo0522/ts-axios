50cc42706be750b1c5c474c6e683ee2710642841
wenruo0522 <zjm023@mail.ustc.edu.cn>
2022-03-11 12:41:53 +0800

    feat: Axios defaults && revise types

--- src/types/index.ts
+++ src/types/index.ts
@@ -44,6 +44,9 @@ export interface AxiosError extends Error {
 }
 
 export interface Axios {
+
+  defaults: AxiosRequestConfig
+
   interceptors: {
     request: AxiosInterceptorManager<AxiosRequestConfig>
     response: AxiosInterceptorManager<AxiosResponse>
