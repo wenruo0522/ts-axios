eb11af6b012332f7eb1f425a60317780cc118edc
wenruo0522 <zjm023@mail.ustc.edu.cn>
2022-03-16 14:22:19 +0800

    feat: basic mergeConfig

--- src/core/mergeConfig.ts
+++ src/core/mergeConfig.ts
@@ -1,5 +1,7 @@
 import { AxiosRequestConfig } from '../types'
 
+const strategys = Object.create(null)
+
 
 function defaultStrategy(val1: any, val2: any): any {
   return typeof val2 !== 'undefined' ? val2 : val1
@@ -14,24 +16,30 @@ function fromVal2Strategy(val1: any, val2: any): any {
 const strategyKeysFromVal2 = ["url", "params", "data"]
 
 strategyKeysFromVal2.forEach(key => {
-  strategy[key] = fromVal2Strategy
+  strategys[key] = fromVal2Strategy
 })
 
 
-function mergeConfig(config1: AxiosRequestConfig, config2: AxiosRequestConfig): AxiosRequestConfig {
+function mergeConfig(config1: AxiosRequestConfig, config2?: AxiosRequestConfig): AxiosRequestConfig {
   if (!config2) {
     config2 = {}
   }
 
   const config = Object.create(null)
 
-  for (let key in config1) {
+  for (let key in config2) {
     mergeField(key)
+  }
+
+  for (let key in config1) {
+    if (!config2[key]) {
+      mergeField(key)
+    }
   }
 
   function mergeField(key: string): void {
     const strategy = strategys[key] || defaultStrategy
-    config[key] = strategy()
+    config[key] = strategy(config1[key], config2![key])
   }
 
   return config
