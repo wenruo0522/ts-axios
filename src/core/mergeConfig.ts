983c06273ffd19804eb766c9a2d1f779b75d7322
wenruo0522 <zjm023@mail.ustc.edu.cn>
2022-03-23 14:16:33 +0800

    feat: deepMergeConfig

--- src/core/mergeConfig.ts
+++ src/core/mergeConfig.ts
@@ -1,4 +1,5 @@
 import { AxiosRequestConfig } from '../types'
+import { isPlainObject, deepMerge } from '../helpers/util'
 
 const strategys = Object.create(null)
 
@@ -12,6 +13,24 @@ function fromVal2Strategy(val1: any, val2: any): any {
     return val2
   }
 }
+
+function deepMergeStrategy(val1: any, val2: any): any {
+  if (isPlainObject(val2)) {
+    return deepMerge(val1, val2)
+  } else if (typeof val2 !== 'undefined') {
+    return val2
+  } else if (isPlainObject(val1)) {
+    return deepMerge(val1)
+  } else if (typeof val1 !== 'undefined') {
+    return val1
+  }
+}
+
+const strategyKeysDeepMerge = ['headers']
+
+strategyKeysDeepMerge.forEach(key => {
+  strategys[key] = deepMergeStrategy
+})
 
 const strategyKeysFromVal2 = ["url", "params", "data"]
 
