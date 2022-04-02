46d16444c6a2257d391b24ad6f3f3557324dd50b
wenruo0522 <zjm023@mail.ustc.edu.cn>
2022-04-02 18:41:24 +0800

    feat: transformRequest && transformResponse

--- src/types/index.ts
+++ src/types/index.ts
@@ -22,6 +22,8 @@ export interface AxiosRequestConfig {
   headers?: any
   responseType?: XMLHttpRequestResponseType
   timeout?: number
+  transformRequest?: AxiosTransformer | AxiosTransformer[]
+  transformResponse?: AxiosTransformer | AxiosTransformer[]
 
   [propName: string]: any
 }
@@ -89,4 +91,7 @@ export interface ResolvedFn<T=any> {
 
 export interface RejectedFn {
   (error: any): any
+}
+export interface AxiosTransformer {
+  (data: any, headers?: any): any
 }
