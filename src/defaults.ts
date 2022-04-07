8108db5726868028008d3f665110c0605dba80a1
wenruo0522 <zjm023@mail.ustc.edu.cn>
2022-04-07 19:58:11 +0800

    feat: fix the transformRequest

--- src/defaults.ts
+++ src/defaults.ts
@@ -13,7 +13,7 @@ const defaults: AxiosRequestConfig = {
 
   transformRequest: [
     function(data: any, headers: any): any {
-      processHeaders(data, headers)
+      processHeaders(headers, data)
       return transformRequest(data)
     }
   ],
